e2e21940649440e8a29b516c05d9bfd6bc2944cd
Li Junchen <lijunchen95@outlook.com>
2024-08-08 22:20:43 +0800

    remove GUI

--- runtimes/native/src/backend/window_minifb.c
+++ runtimes/native/src/backend/window_minifb.c
@@ -1,4 +1,3 @@
-#include <MiniFB.h>
 #include <stdio.h>
 #include "svpng.h"
 
@@ -20,80 +19,25 @@ static void onResize (struct mfb_window* window, int width, int height) {
 }
 
 void w4_windowBoot (const char* title) {
-    struct mfb_window* window = mfb_open_ex(title, viewportSize, viewportSize, WF_RESIZABLE);
-
-    mfb_set_resize_callback(window, onResize);
-
     do {
-        // Keyboard handling
-        const uint8_t* keyBuffer = mfb_get_key_buffer(window);
 
         // Player 1
         uint8_t gamepad = 0;
-        if (keyBuffer[KB_KEY_X] || keyBuffer[KB_KEY_V] || keyBuffer[KB_KEY_K] || keyBuffer[KB_KEY_SPACE]) {
-            gamepad |= W4_BUTTON_X;
-        }
-        if (keyBuffer[KB_KEY_Z] || keyBuffer[KB_KEY_C] || keyBuffer[KB_KEY_Y] || keyBuffer[KB_KEY_W] || keyBuffer[KB_KEY_J]) {
-            gamepad |= W4_BUTTON_Z;
-        }
-        if (keyBuffer[KB_KEY_LEFT]) {
-            gamepad |= W4_BUTTON_LEFT;
-        }
-        if (keyBuffer[KB_KEY_RIGHT]) {
-            gamepad |= W4_BUTTON_RIGHT;
-        }
-        if (keyBuffer[KB_KEY_UP]) {
-            gamepad |= W4_BUTTON_UP;
-        }
-        if (keyBuffer[KB_KEY_DOWN]) {
-            gamepad |= W4_BUTTON_DOWN;
-        }
         w4_runtimeSetGamepad(0, gamepad);
 
         // Player 2
         gamepad = 0;
-        if (keyBuffer[KB_KEY_LEFT_SHIFT] || keyBuffer[KB_KEY_TAB]) {
-            gamepad |= W4_BUTTON_X;
-        }
-        if (keyBuffer[KB_KEY_A] || keyBuffer[KB_KEY_Q]) {
-            gamepad |= W4_BUTTON_Z;
-        }
-        if (keyBuffer[KB_KEY_S]) {
-            gamepad |= W4_BUTTON_LEFT;
-        }
-        if (keyBuffer[KB_KEY_F]) {
-            gamepad |= W4_BUTTON_RIGHT;
-        }
-        if (keyBuffer[KB_KEY_E]) {
-            gamepad |= W4_BUTTON_UP;
-        }
-        if (keyBuffer[KB_KEY_D]) {
-            gamepad |= W4_BUTTON_DOWN;
-        }
         w4_runtimeSetGamepad(1, gamepad);
 
         // Mouse handling
         uint8_t mouseButtons = 0;
-        const uint8_t* mouseBuffer = mfb_get_mouse_button_buffer(window);
-        if (mouseBuffer[MOUSE_LEFT]) {
-            mouseButtons |= W4_MOUSE_LEFT;
-        }
-        if (mouseBuffer[MOUSE_RIGHT]) {
-            mouseButtons |= W4_MOUSE_RIGHT;
-        }
-        if (mouseBuffer[MOUSE_MIDDLE]) {
-            mouseButtons |= W4_MOUSE_MIDDLE;
-        }
-        int mouseX = mfb_get_mouse_x(window);
-        int mouseY = mfb_get_mouse_y(window);
+        int mouseX = mfb_get_mouse_x(0);
+        int mouseY = mfb_get_mouse_y(0);
         w4_runtimeSetMouse(160*(mouseX-viewportX)/viewportSize, 160*(mouseY-viewportY)/viewportSize, mouseButtons);
 
         w4_runtimeUpdate();
 
-        if (mfb_update_ex(window, pixels, 160, 160) < 0) {
-            break;
-        }
-    } while (mfb_wait_sync(window));
+    } while (1);
 }
 
 int counter = 0;
--- runtimes/native/src/window.h
+++ runtimes/native/src/window.h
@@ -2,6 +2,4 @@
 
 #include <stdint.h>
 
-void w4_windowBoot (const char* title);
-
 void w4_windowComposite (const uint32_t* palette, const uint8_t* framebuffer);
